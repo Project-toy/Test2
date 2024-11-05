47129da763d08d3e6e0c90b5806b7b1d2b7eca1d
lee sunwoo <so8907@naver.com>
2024-11-05 22:23:58 +0900

    안녕

--- WebTest/print.js
+++ WebTest/print.js
@@ -1,10 +1,39 @@
-var figlet = require("figlet");
+async function getSummaryText(text) {
+  const docObject = {  //요약할 문서
+    content: "text"
+  };
 
-figlet("hello world!", function (err, data) {
-    if (err) {
-      console.log("Something went wrong...");
-      console.dir(err);
-      return;
-    }
+  const OptionObject = {
+    language: "ko",
+    model: "general" ,
+    tone: 2,
+    summaryCount: 3,
+  };
+
+
+  // Option과 docObject를 합쳐서 요청으로 보낼 데이터 생성
+  const requestData = {
+    document: docObject,
+    option: OptionObject,
+  };
+  const requestOptions = { //요청 헤더
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+        "X-NCP-APIGW-API-KEY-ID": "462uzstacg", // API 키 ID
+        "X-NCP-APIGW-API-KEY": "ejrkxgIBimFdCJl2FKln7hLrVRTUGufolqqLE50H" // API 키 Secret
+      },
+      body: JSON.stringify(requestData)
+    }; 
+
+  try {
+    const response = await fetch(`https://naveropenapi.apigw.ntruss.com/text-summary/v1/summarize`, requestOptions);
+    const data = await response.json();
     console.log(data);
-  });
+    return data; // 변환된 텍스트 결과 반환
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+}
